01b4b2c02fc867a14cb982dad4d38ae01e62d78c
Pierre de Buyl <pdebuyl@nononospam.seriously.ulb.ac.be>
2011-02-17 10:42:16 -0500

    Moved sample configs to examples/ , moved python files to py.

---
+++ py/disp_v_xyz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax1 = fig.add_subplot(311)
+ax2 = fig.add_subplot(312)
+ax3 = fig.add_subplot(313)
+
+a = np.loadtxt('at_v')
+for i in range(a.shape[1]/3):
+    ax1.plot(a[:,3*i])
+    ax2.plot(a[:,1+3*i])
+    ax3.plot(a[:,2+3*i])
+
+plt.show()
