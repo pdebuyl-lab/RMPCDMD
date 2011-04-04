e18b2fe5d68ae7c9f20e2112b90db59f9b1c49f2
Pierre de Buyl <pdebuyl@nononospam.seriously.ulb.ac.be>
2011-04-04 10:08:16 -0400

    Added single-obs arguments to scripts/disp_obs.py .

--- scripts/disp_obs.py
+++ scripts/disp_obs.py
@@ -1,20 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
+from sys import argv
 
 a = h5py.File('data.h5', 'r')
 
 all_obs = a['observables'].keys()
 
-for o in all_obs:
-    try:
-        d_set = a['observables'][o]['samples']
-        t_set = a['observables'][o]['time']
+if (len(argv) > 1):
+    o = argv[1]
+    d_set = a['observables'][o]['samples']
+    t_set = a['observables'][o]['time']
+    plt.plot(t_set, d_set, label=o)
 
-        if (len(d_set.shape)==1):
-            plt.plot(t_set, d_set[:] - d_set[0], label=o)
-    except:
-        print o, 'not a good observable'
+else:
+
+    for o in all_obs:
+        try:
+            d_set = a['observables'][o]['samples']
+            t_set = a['observables'][o]['time']
+            
+            if (len(d_set.shape)==1):
+                plt.plot(t_set, d_set[:] - d_set[0], label=o)
+        except:
+            print o, 'not a good observable'
 
 a.close()
 
