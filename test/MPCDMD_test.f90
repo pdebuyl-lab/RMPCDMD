25277bd41023cabeccca0693935efb700b93bc4e
Pierre de Buyl <pdebuyl@nononospam.seriously.ulb.ac.be>
2012-05-30 12:06:41 +0200

    Set cutoff for collisions to cut+sqrt(3)*a/2

--- test/MPCDMD_test.f90
+++ test/MPCDMD_test.f90
@@ -612,7 +612,7 @@ program test
            do i=1,N_groups
               do j=1,group_list(i) % N
                  idx1 = group_list(i) % istart + j - 1
-                 call switch_off(at_r(:,idx1), maxval(at_so % sig(at_species(idx1),:))+sqrt(3.d0)/2.d0*a, maxval(at_so % cut(at_species(idx1),:))+a)
+                 call switch_off(at_r(:,idx1), maxval(at_so % cut(at_species(idx1),:))+sqrt(3.d0)/2.d0*a, maxval(at_so % cut(at_species(idx1),:))+a)
               end do
            end do
         end if
