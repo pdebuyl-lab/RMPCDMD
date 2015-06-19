7a170dfa5750e6146dee28441090c301daa8beeb
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-06-19 14:04:52 +0200

    add other variables: vel, force, id, species

--- src/particle_system.f90
+++ src/particle_system.f90
@@ -11,9 +11,28 @@ module particle_system
      double precision, pointer :: pos(:,:)
      double precision, pointer :: pos_old(:,:)
      double precision, pointer :: pos_pointer(:,:)
+     double precision, pointer :: vel1(:,:)
+     double precision, pointer :: vel2(:,:)
+     double precision, pointer :: vel(:,:)
+     double precision, pointer :: vel_old(:,:)
+     double precision, pointer :: vel_pointer(:,:)
+     double precision, pointer :: force1(:,:)
+     double precision, pointer :: force2(:,:)
+     double precision, pointer :: force(:,:)
+     double precision, pointer :: force_old(:,:)
+     double precision, pointer :: force_pointer(:,:)
+     integer, pointer :: id1(:)
+     integer, pointer :: id2(:)
+     integer, pointer :: id(:)
+     integer, pointer :: id_old(:)
+     integer, pointer :: id_pointer(:)
+     integer, pointer :: species1(:)
+     integer, pointer :: species2(:)
+     integer, pointer :: species(:)
+     integer, pointer :: species_old(:)
+     integer, pointer :: species_pointer(:)
    contains
      procedure :: init
-     procedure :: del
   end type particle_system_t
 
 contains
@@ -25,21 +44,29 @@ contains
     this% Nmax = Nmax
     allocate(this% pos1(3, Nmax))
     allocate(this% pos2(3, Nmax))
-
     this% pos => this% pos1
     this% pos_old => this% pos2
 
+    allocate(this% vel1(3, Nmax))
+    allocate(this% vel2(3, Nmax))
+    this% vel => this% vel1
+    this% vel_old => this% vel2
+
+    allocate(this% force1(3, Nmax))
+    allocate(this% force2(3, Nmax))
+    this% force => this% force1
+    this% force_old => this% force2
+
+    allocate(this% id1(Nmax))
+    allocate(this% id2(Nmax))
+    this% id => this% id1
+    this% id_old => this% id2
+
+    allocate(this% species1(Nmax))
+    allocate(this% species2(Nmax))
+    this% species => this% species1
+    this% species_old => this% species2
+
   end subroutine init
-
-  subroutine del(this)
-    class(particle_system_t), intent(inout) :: this
-
-    deallocate(this% pos1)
-    deallocate(this% pos2)
-    this% pos => null()
-    this% pos_old => null()
-    this% pos_pointer => null()
-
-  end subroutine del
 
 end module particle_system
--- test/try_all.f90
+++ test/try_all.f90
@@ -15,6 +15,9 @@ program try_all
 
   call solvent% init(N)
 
+  call random_number(solvent% vel(:, i))
+  solvent% force = 0
+  solvent% species = 1
   do i = 1, N
      call random_number(solvent% pos(:, i))
      do j=1, 3
@@ -47,11 +50,8 @@ program try_all
   do j = 1, 3
      solvent% pos(j, :) = solvent% pos(j, :) * L(j)
   end do
-  call solvent_cells%count_particles(solvent% pos)
-  call solvent_cells%sort_particles(solvent% pos, solvent% pos_old)
-  solvent% pos_pointer => solvent% pos
-  solvent% pos => solvent% pos_old
-  solvent% pos_old => solvent% pos_pointer
+
+  call sort
 
   open(12, file='sorted_pos_2')
   do i=1, N
@@ -59,5 +59,45 @@ program try_all
   end do
   close(12)
 
+contains
+
+  subroutine sort
+    integer :: i, idx, start, p(3)
+
+    call solvent_cells%count_particles(solvent% pos)
+
+    do i=1, N
+       p = floor( (solvent% pos(:, i) - solvent_cells% origin ) / solvent_cells% a )
+       idx = compact_p_to_h(p, solvent_cells% M) + 1
+       start = solvent_cells% cell_start(idx)
+       solvent% pos_old(:, start) = solvent% pos(:, i)
+       solvent% vel_old(:, start) = solvent% vel(:, i)
+       solvent% force_old(:, start) = solvent% force(:, i)
+       solvent% id_old(start) = solvent% id(i)
+       solvent% species_old(start) = solvent% species(i)
+       solvent_cells% cell_start(idx) = start + 1
+    end do
+
+    solvent% pos_pointer => solvent% pos
+    solvent% pos => solvent% pos_old
+    solvent% pos_old => solvent% pos_pointer
+
+    solvent% vel_pointer => solvent% vel
+    solvent% vel => solvent% vel_old
+    solvent% vel_old => solvent% vel_pointer
+
+    solvent% force_pointer => solvent% force
+    solvent% force => solvent% force_old
+    solvent% force_old => solvent% force_pointer
+
+    solvent% id_pointer => solvent% id
+    solvent% id => solvent% id_old
+    solvent% id_old => solvent% id_pointer
+
+    solvent% species_pointer => solvent% species
+    solvent% species => solvent% species_old
+    solvent% species_old => solvent% species_pointer
+
+  end subroutine sort
 
 end program try_all
