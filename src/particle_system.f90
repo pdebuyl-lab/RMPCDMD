6565b4cd7a422728edeab9ea936235a58dd7a5a0
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-06-21 09:52:24 +0200

    add rel_pos

--- src/particle_system.f90
+++ src/particle_system.f90
@@ -83,4 +83,22 @@ contains
 
   end subroutine random_placement
 
+  function rel_pos(x, y, L) result(r)
+    double precision, intent(in) :: x(3), y(3), L(3)
+
+    double precision :: r(3)
+    integer :: i, dim
+
+    r = x - y
+
+    do dim=1,3
+       if ( r(dim) < -0.5d0*r(dim) ) then
+          r(dim) = r(dim) + L(dim)
+       else if ( r(dim) > 0.5d0*L(dim) ) then
+          r(dim) = r(dim) - L(dim)
+       end if
+    end do
+
+  end function rel_pos
+
 end module particle_system
