bb39c757b3cba9f78efedb94181410a6679d6252
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-06-28 13:51:15 +0200

    increase accuracy of cell_system testing

--- test/test_cell_system_0.f90
+++ test/test_cell_system_0.f90
@@ -13,6 +13,7 @@ program try_all
 
   integer :: i, L(3), seed_size, clock
   integer, allocatable :: seed(:)
+  integer :: h, c1(3), c2(3)
 
   call random_seed(size = seed_size)
   allocate(seed(seed_size))
@@ -35,13 +36,30 @@ program try_all
 
   call solvent_cells%count_particles(pos)
 
+  call sort
+  call solvent_cells%count_particles(pos)
+
   success = .true.
-  if (sum(solvent_cells%cell_count) .ne. N) success = .false.
-  print *, sum(solvent_cells%cell_count), N
   if (solvent_cells%cell_start(1) .ne. 1) success = .false.
   print *, solvent_cells%cell_start(1), 1
   if (solvent_cells%cell_start(solvent_cells%N) .ne. N+1) success = .false.
   print *, solvent_cells%cell_start(solvent_cells%N), N+1
+  if (sum(solvent_cells%cell_count) .ne. N) success = .false.
+  print *, sum(solvent_cells%cell_count), N
+
+  h = 1
+  do i = 1, N
+     if (i .ge. solvent_cells% cell_start(h) + solvent_cells% cell_count(h)) h = h + 1
+     do while (solvent_cells% cell_count(h) .eq. 0)
+        h = h+1
+     end do
+     c1 = floor( (pos(:, i) - solvent_cells% origin) / solvent_cells% a )
+     c2 = compact_h_to_p(h-1, solvent_cells% M)
+     if (c1(1) .ne. c2(1)) success = .false.
+     if (c1(2) .ne. c2(2)) success = .false.
+     if (c1(3) .ne. c2(3)) success = .false.
+     if (.not. success) print *, i, h, c1, c2
+  end do
 
   if (success) write(*,*) 'success'
 
