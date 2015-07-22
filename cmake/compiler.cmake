34a873c1e1f705504ceb755671ca9f2da70a7720
Peter Colberg <peter@colberg.org>
2015-07-22 16:08:41 -0400

    require Fortran 2008 when using gfortran

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_BUILD_TYPE_INIT "Release")
 
 if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
-  set(CMAKE_Fortran_FLAGS_INIT "-Wall -Wextra -Wconversion")
+  set(CMAKE_Fortran_FLAGS_INIT "-Wall -Wextra -Wconversion -std=f2008 -pedantic")
 endif()
