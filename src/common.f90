3692621ee190a65b22c91b485faec4ff83368461
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-09-07 14:35:00 +0200

    add histogram_t

--- src/common.f90
+++ src/common.f90
@@ -4,6 +4,7 @@ module common
 
   public :: rel_pos
   public :: profile_t
+  public :: histogram_t
 
   type profile_t
      double precision, allocatable :: data(:)
@@ -17,6 +18,18 @@ module common
      generic, public :: bin => profile_bin
      procedure, private :: profile_bin
   end type profile_t
+
+  type histogram_t
+     double precision, allocatable :: data(:)
+     double precision :: xmin
+     double precision :: dx
+     integer :: n
+   contains
+     generic, public :: init => histogram_init
+     procedure, private :: histogram_init
+     generic, public :: bin => histogram_bin
+     procedure, private :: histogram_bin
+  end type histogram_t
 
 contains
 
@@ -67,5 +80,32 @@ contains
     end if
 
   end subroutine profile_bin
+
+  subroutine histogram_init(this, xmin, xmax, n)
+    class(histogram_t), intent(out) :: this
+    double precision, intent(in) :: xmin, xmax
+    integer, intent(in) :: n
+
+    this% n = n
+    this% xmin = xmin
+    this% dx = (xmax - xmin) / n
+    if (this% dx <= 0) error stop 'negative step in histogram_init'
+    allocate(this% data(n))
+    this% data = 0
+
+  end subroutine histogram_init
+
+  subroutine histogram_bin(this, x)
+    class(histogram_t), intent(inout) :: this
+    double precision, intent(in) :: x
+
+    integer :: idx
+
+    idx = floor( (x - this% xmin) / this% dx ) + 1
+    if ( ( idx > 0 ) .and. ( idx <= this% n ) ) then
+       this% data(idx) = this% data(idx) + 1
+    end if
+
+  end subroutine histogram_bin
 
 end module common
