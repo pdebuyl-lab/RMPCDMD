c475bda2a91e73b337883b8330cab53e7a4e5ded
Pierre de Buyl <pdebuyl@pdebuyl.be>
2016-01-06 15:21:10 +0100

    use thread local memory for particle counting

--- src/cell_system.f90
+++ src/cell_system.f90
@@ -13,6 +13,7 @@ module cell_system
      double precision :: a
      double precision :: origin(3)
      integer, allocatable :: cell_count(:)
+     integer, allocatable :: cell_count_tmp(:,:)
      integer, allocatable :: cell_start(:)
      integer :: M(3)
      logical :: has_walls
@@ -25,6 +26,7 @@ module cell_system
 contains
 
   subroutine init(this, L, a, has_walls)
+    use omp_lib
     class(cell_system_t), intent(out) :: this
     integer, intent(in) :: L(3)
     double precision, intent(in) :: a
@@ -53,27 +55,32 @@ contains
     end do
     this%N = 2**this%M(1)*2**this%M(2)*2**this%M(3)
     allocate(this%cell_count(this%N))
+    allocate(this%cell_count_tmp(this%N, omp_get_max_threads()))
     allocate(this%cell_start(this%N))
 
   end subroutine init
 
   subroutine count_particles(this, position)
+    use omp_lib
     class(cell_system_t), intent(inout) :: this
     double precision, intent(in) :: position(:, :)
 
-    integer :: i, idx, N_particles
+    integer :: i, idx, N_particles, t_id, t_max
     integer :: p(3)
     integer :: L(3)
     logical :: nowalls
 
     N_particles = size(position, 2)
+    t_max = omp_get_max_threads()
 
     L = this% L
     nowalls = .not. this% has_walls
 
-    this%cell_count = 0
+    this%cell_count_tmp = 0
 
-    !$omp parallel do private(p, idx)
+    !$omp parallel private(i, p, idx, t_id)
+    t_id = omp_get_thread_num() + 1
+    !$omp do
     do i=1, N_particles
        p = floor( (position(:, i) / this% a ) - this%origin )
        if ( p(1) == L(1) ) p(1) = 0
@@ -82,8 +89,17 @@ contains
           if ( p(3) == L(3) ) p(3) = 0
        end if
        idx = compact_p_to_h(p, this%M) + 1
-       !$omp atomic
-       this%cell_count(idx) = this%cell_count(idx) + 1
+       this%cell_count_tmp(idx, t_id) = this%cell_count_tmp(idx, t_id) + 1
+    end do
+    !$omp end do
+    !$omp end parallel
+
+    !$omp parallel do private(t_id)
+    do i = 1, this%N
+       this%cell_count(i) = 0
+       do t_id=1, t_max
+          this%cell_count(i) = this%cell_count(i) + this%cell_count_tmp(i, t_id)
+       end do
     end do
 
     this%cell_start(1) = 1
