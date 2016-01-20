5e6c308d706619cdfaf4a07f6328946233fd0e0b
Pierre de Buyl <pdebuyl@pdebuyl.be>
2016-01-20 15:05:29 +0100

    specify c99 standard

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -2,8 +2,10 @@ set(CMAKE_BUILD_TYPE_INIT "Release")
 
 if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
   set(CMAKE_Fortran_FLAGS_INIT "-fopenmp -Wall -Wextra -Wconversion -std=f2008 -pedantic")
+  set(CMAKE_C_FLAGS_INIT "-Wall -std=c99")
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL "Intel")
   set(CMAKE_Fortran_FLAGS_INIT "-openmp -stand f08")
+  set(CMAKE_C_FLAGS_INIT "-std=c99")
 endif()
 
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,--as-needed")
