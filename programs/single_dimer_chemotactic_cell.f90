7e2f4ea725169b57ce1eaadff2442390bdefd8f6
Pierre de Buyl <pdebuyl@pdebuyl.be>
2016-02-17 20:58:35 +0100

    increase width of timers display

--- programs/single_dimer_chemotactic_cell.f90
+++ programs/single_dimer_chemotactic_cell.f90
@@ -418,20 +418,20 @@ program setup_single_dimer
   call hfile%close()
   call h5close_f(error)
 
-  write(*,'(a16,f8.3)') solvent%time_stream%name, solvent%time_stream%total
-  write(*,'(a16,f8.3)') solvent%time_md_vel%name, solvent%time_md_vel%total
-  write(*,'(a16,f8.3)') solvent%time_step%name, solvent%time_step%total
-  write(*,'(a16,f8.3)') solvent%time_count%name, solvent%time_count%total
-  write(*,'(a16,f8.3)') solvent%time_sort%name, solvent%time_sort%total
-  write(*,'(a16,f8.3)') solvent%time_ct%name, solvent%time_ct%total
-  write(*,'(a16,f8.3)') solvent%time_max_disp%name, solvent%time_max_disp%total
-  write(*,'(a16,f8.3)') flag_timer%name, flag_timer%total
-  write(*,'(a16,f8.3)') change_timer%name, buffer_timer%total
-  write(*,'(a16,f8.3)') buffer_timer%name, buffer_timer%total
-  write(*,'(a16,f8.3)') neigh%time_update%name, neigh%time_update%total
-  write(*,'(a16,f8.3)') neigh%time_force%name, neigh%time_force%total
+  write(*,'(a16,f15.3)') solvent%time_stream%name, solvent%time_stream%total
+  write(*,'(a16,f15.3)') solvent%time_md_vel%name, solvent%time_md_vel%total
+  write(*,'(a16,f15.3)') solvent%time_step%name, solvent%time_step%total
+  write(*,'(a16,f15.3)') solvent%time_count%name, solvent%time_count%total
+  write(*,'(a16,f15.3)') solvent%time_sort%name, solvent%time_sort%total
+  write(*,'(a16,f15.3)') solvent%time_ct%name, solvent%time_ct%total
+  write(*,'(a16,f15.3)') solvent%time_max_disp%name, solvent%time_max_disp%total
+  write(*,'(a16,f15.3)') flag_timer%name, flag_timer%total
+  write(*,'(a16,f15.3)') change_timer%name, buffer_timer%total
+  write(*,'(a16,f15.3)') buffer_timer%name, buffer_timer%total
+  write(*,'(a16,f15.3)') neigh%time_update%name, neigh%time_update%total
+  write(*,'(a16,f15.3)') neigh%time_force%name, neigh%time_force%total
 
-  write(*,'(a16,f8.3)') 'total                          ', &
+  write(*,'(a16,f15.3)') 'total                          ', &
        solvent%time_stream%total + solvent%time_step%total + solvent%time_count%total +&
        solvent%time_sort%total + solvent%time_ct%total + solvent%time_md_vel%total + solvent%time_max_disp%total + &
        flag_timer%total + change_timer%total + buffer_timer%total + neigh%time_update%total + neigh%time_force%total
