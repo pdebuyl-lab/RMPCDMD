e2db004c264a1224faded40a41ae953ab567141f
Laurens Deprez <laurens.deprez@student.kuleuven.be>
2016-02-18 14:43:33 +0100

    add shift, energy_9_3, force_9_3

--- src/interaction.f90
+++ src/interaction.f90
@@ -8,6 +8,8 @@ module interaction
   public :: lj_energy
   public :: lj_force_9_6
   public :: lj_energy_9_6
+  public :: lj_force_9_3
+  public :: lj_energy_9_3
 
   type lj_params_t
      integer :: n1, n2
@@ -16,17 +18,19 @@ module interaction
      double precision, allocatable :: sigma_sq(:,:)
      double precision, allocatable :: cut(:,:)
      double precision, allocatable :: cut_sq(:,:)
+     double precision, allocatable :: shift
    contains
      procedure :: init => lj_params_init
   end type lj_params_t
 
 contains
 
-  subroutine lj_params_init(this, epsilon, sigma, cut)
+  subroutine lj_params_init(this, epsilon, sigma, cut, shift)
     class(lj_params_t), intent(out) :: this
     double precision, intent(in) :: epsilon(:,:)
     double precision, intent(in) :: sigma(:,:)
     double precision, intent(in) :: cut(:,:)
+    double precision, intent(in), optional :: shift
 
     integer :: n1, n2
 
@@ -47,13 +51,18 @@ contains
     allocate(this% sigma_sq(n1, n2))
     allocate(this% cut(n1, n2))
     allocate(this% cut_sq(n1, n2))
+    allocate(this% shift)
 
     this% epsilon = epsilon
     this% sigma = sigma
     this% sigma_sq = sigma**2
     this% cut = cut
     this% cut_sq = cut**2
-
+    if (present(shift)) then
+       this% shift = shift
+    else
+       this% shift = 0.d0
+    end if
   end subroutine lj_params_init
 
 
@@ -88,6 +97,18 @@ contains
 
   end function lj_force_9_6
 
+  pure function lj_force_9_3(z, z_sq, epsilon, sigma) result(e)
+     double precision, intent(in) ::z, z_sq, epsilon, sigma
+     double precision :: e
+
+     double precision :: sig3_o_r3, force_o_z
+     
+     sig3_o_r3 = sigma**3/z_sq**(3.d0/2.d0)
+
+     force_o_z = 9*sqrt(3.d0)/2*epsilon/z_sq* (3.d0*sig3_o_r3**3 - sig3_o_r3)
+     e = force_o_z*z
+  end function lj_force_9_3
+
   pure function lj_energy(r_sq, epsilon, sigma) result(e)
     double precision, intent(in) :: r_sq, epsilon, sigma
     double precision :: e
@@ -112,5 +133,16 @@ contains
     e = 6.75d0*epsilon * (sig6_o_r6*(sig3_o_r3 - 1.d0) + 4.d0/27.d0)
 
   end function lj_energy_9_6
+
+  pure function lj_energy_9_3(z_sq, epsilon, sigma) result(e)
+     double precision, intent(in) :: z_sq, epsilon, sigma
+     double precision :: e
+
+     double precision :: sig3_o_r3
+     
+     sig3_o_r3 = sigma**3/z_sq**(3.d0/2.d0)
+
+     e = 3*sqrt(3.d0)/2*epsilon*(sig3_o_r3**3 - sig3_o_r3) + epsilon
+  end function lj_energy_9_3
 
 end module interaction
