8b4583a2f93444a83fd19bde4bec1ea3af614420
Pierre de Buyl <pdebuyl@pdebuyl.be>
2016-05-25 14:13:53 +0200

    update setup_simple_colloids
    Fix initialization of the RNG.
    Use a density parameter rho.
    Fix use of apply_pbc before sorting.
    Use skin variable consistently.

--- test/setup_simple_colloids.f90
+++ test/setup_simple_colloids.f90
@@ -21,10 +21,12 @@ program setup_simple_colloids
   type(lj_params_t) :: solvent_colloid_lj
   type(lj_params_t) :: colloid_lj
 
-  integer, parameter :: N = 3000
+  integer, parameter :: rho = 10
+  double precision, parameter :: sigma = 1
+  double precision, parameter :: sigma_cut = sigma*2**(1.d0/6.d0)
   integer :: error
 
-  double precision :: epsilon, sigma, sigma_cut
+  double precision :: epsilon
   double precision :: mass
   double precision :: so_max, co_max
 
@@ -39,45 +41,37 @@ program setup_simple_colloids
   double precision :: tmp_x(3)
   double precision :: skin
 
-  integer :: i, L(3)
+  integer :: i, L(3), N
   integer :: j, k
 
   n_threads = omp_get_max_threads()
   allocate(state(n_threads))
-
-  do i = 1, n_threads
-     state(i)%counter%c0 = 0
-     state(i)%counter%c1 = 0
-     state(i)%key%c0 = 0
-     state(i)%key%c1 = 719287321987291_c_long
-  end do
+  call threefry_rng_init(state, 719287321987291_c_long)
 
   call h5open_f(error)
-
-  L = [12, 12, 12]
-
-  epsilon = 1
-  sigma = 1
-  sigma_cut = sigma*2**(1.d0/6.d0)
-
-  call solvent_colloid_lj% init( reshape( [ epsilon ], [1, 1] ), &
-       reshape( [ sigma ], [1, 1] ), reshape( [ sigma_cut ], [1, 1] ) )
-
-  epsilon = 1
-  sigma = 1
-  sigma_cut = sigma*2**(1.d0/6.d0)
-
-  call colloid_lj% init( reshape( [ epsilon ], [1, 1] ), &
-       reshape( [ sigma ], [1, 1] ), reshape( [ sigma_cut ], [1, 1] ) )
-
-  mass = 3000.d0 / (L(1)*L(2)*L(3)) * sigma**3 * 4 * 3.141/3
-
-  call solvent% init(N)
 
   call colloids% init_from_file('input_data.h5', 'colloids', H5MD_LINEAR, 4)
   write(*, *) colloids% pos
   colloids% species = 1
   colloids% vel = 0
+
+  L = 10
+  N = rho*L(1)*L(2)*L(3) - int(rho*4*3.142/3 * sigma**3*colloids%Nmax)
+  if (N <= 0) error stop 'Not enough volume available for solvent'
+
+  epsilon = 1
+
+  call solvent_colloid_lj% init( reshape( [ epsilon ], [1, 1] ), &
+       reshape( [ sigma ], [1, 1] ), reshape( [ sigma_cut ], [1, 1] ) )
+
+  epsilon = 1
+
+  call colloid_lj% init( reshape( [ epsilon ], [1, 1] ), &
+       reshape( [ 2*sigma ], [1, 1] ), reshape( [ 2*sigma_cut ], [1, 1] ) )
+
+  mass = 3000.d0 / (L(1)*L(2)*L(3)) * sigma**3 * 4 * 3.141/3
+
+  call solvent% init(N)
 
   call random_number(solvent% vel(:, :))
   solvent% vel = (solvent% vel - 0.5d0)*sqrt(6.d0*2)
@@ -91,9 +85,7 @@ program setup_simple_colloids
 
   call solvent% sort(solvent_cells)
 
-  call neigh% init(colloids% Nmax, 300)
-  call neigh% make_stencil(solvent_cells, 1.5d0)
-
+  call neigh% init(colloids% Nmax, 500)
 
   tau = 0.1d0
   N_MD_steps = 100
@@ -102,7 +94,8 @@ program setup_simple_colloids
   skin = 0.8
   n_extra_sorting = 0
 
-  call neigh% update_list(colloids, solvent, sigma_cut + skin, solvent_cells)
+  call neigh% make_stencil(solvent_cells, sigma_cut+skin)
+  call neigh% update_list(colloids, solvent, sigma_cut+skin, solvent_cells)
 
   e1 = compute_force(colloids, solvent, neigh, solvent_cells% edges, solvent_colloid_lj)
   e2 = compute_force_n2(colloids, solvent_cells% edges, colloid_lj)
@@ -115,29 +108,23 @@ program setup_simple_colloids
 
   solvent% pos_old = solvent% pos
   colloids% pos_old = colloids% pos
-  do i = 1, 100
-     so_max = 0
-     co_max = 0
+  do i = 1, 2000
      md_loop: do j = 1, N_MD_steps
-        !$omp parallel do private(k, tmp_x)
-        do k = 1, solvent% Nmax
-           solvent% pos(:,k) = solvent% pos(:,k) + dt * solvent% vel(:,k) + dt**2 * solvent% force(:,k) / 2
-        end do
+        call md_pos(solvent, dt)
         do k = 1, colloids% Nmax
            colloids% pos(:,k) = colloids% pos(:,k) + dt * colloids% vel(:,k) + dt**2 * colloids% force(:,k) / (2 * mass)
         end do
         so_max = solvent% maximum_displacement()
         co_max = colloids% maximum_displacement()
 
-        if ( (co_max >= skin/2) .or. (so_max >= skin/2) ) then
+        if ( (co_max >= skin*0.1) .or. (so_max >= skin*0.9) ) then
            call apply_pbc(solvent, solvent_cells% edges)
            call apply_pbc(colloids, solvent_cells% edges)
            call solvent% sort(solvent_cells)
-           call neigh% update_list(colloids, solvent, sigma_cut + skin, solvent_cells)
+           call neigh% update_list(colloids, solvent, sigma_cut+skin, solvent_cells)
            solvent% pos_old = solvent% pos
            colloids% pos_old = colloids% pos
            n_extra_sorting = n_extra_sorting + 1
-           write(*,*) 'extra sorting'
         end if
 
         call switch(solvent% force, solvent% force_old)
@@ -147,16 +134,15 @@ program setup_simple_colloids
         e1 = compute_force(colloids, solvent, neigh, solvent_cells% edges, solvent_colloid_lj)
         e2 = compute_force_n2(colloids, solvent_cells% edges, colloid_lj)
 
-        !$omp parallel do private(k)
-        do k = 1, solvent% Nmax
-           solvent% vel(:,k) = solvent% vel(:,k) + dt * ( solvent% force(:,k) + solvent% force_old(:,k) ) / 2
-        end do
+        call md_vel(solvent, dt)
         colloids% vel = colloids% vel + dt * ( colloids% force + colloids% force_old ) / (2 * mass)
 
         write(15,*) colloids% pos + colloids% image * spread(solvent_cells% edges, dim=2, ncopies=colloids% Nmax)
 
      end do md_loop
 
+     call apply_pbc(solvent, solvent_cells% edges)
+     call apply_pbc(colloids, solvent_cells% edges)
      call solvent% sort(solvent_cells)
      call neigh% update_list(colloids, solvent, sigma_cut + skin, solvent_cells)
      solvent% pos_old = solvent% pos
@@ -164,6 +150,7 @@ program setup_simple_colloids
 
      call simple_mpcd_step(solvent, solvent_cells, state)
 
+     if (modulo(i,10)==0) &
      write(*,'(6f16.3)') e1, e2, mass*sum(colloids% vel**2)/2, sum(solvent% vel**2)/2, &
          e1+e2+mass*sum(colloids% vel**2)/2+sum(solvent% vel**2)/2, &
          compute_temperature(solvent, solvent_cells)
