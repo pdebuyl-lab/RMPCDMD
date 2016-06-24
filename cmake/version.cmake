3a4a7b1d674c3130d434e26b1106645e5891d25f
Pierre de Buyl <pdebuyl@pdebuyl.be>
2016-06-24 15:44:17 +0200

    remove unnecessary output

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,10 +1,7 @@
 FIND_PACKAGE(Git)
 
-message("hop")
-message(${CMAKE_SOURCE_DIR}/..)
-
 IF(GIT_EXECUTABLE)
-  execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags --dirty OUTPUT_VARIABLE RMPCDMD_REVISION OUTPUT_STRIP_TRAILING_WHITESPACE)
+  execute_process(COMMAND ${GIT_EXECUTABLE} describe --always --tags --dirty OUTPUT_VARIABLE RMPCDMD_REVISION OUTPUT_STRIP_TRAILING_WHITESPACE)
   configure_file(${CMAKE_SOURCE_DIR}/../src/rmpcdmd_module.f90.in ${CMAKE_BINARY_DIR}/rmpcdmd_module.f90 @ONLY)
 else()
   message(FATAL_ERROR "git not found")
