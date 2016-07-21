c98d1184bbebab1b219c75d0039ff0fa3c1b482e
Pierre de Buyl <pdebuyl@pdebuyl.be>
2016-07-21 17:52:43 +0200

    add test of dimer simulation

---
+++ test/dimer_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from __future__ import print_function, division
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file', type=str, help='H5MD datafile')
+args = parser.parse_args()
+
+import numpy as np
+import h5py
+
+with h5py.File(args.file, 'r') as f:
+    internal_energy = f['observables/internal_energy/value'][:]
+    internal_energy -= internal_energy.mean()
+    assert internal_energy.std() < 0.1
+
+    center_of_mass_velocity = f['observables/center_of_mass_velocity/value'][:]
+    center_of_mass_velocity -= center_of_mass_velocity.mean(axis=0).reshape((1,3))
+    assert np.max(np.abs(center_of_mass_velocity)) < 1e-15
