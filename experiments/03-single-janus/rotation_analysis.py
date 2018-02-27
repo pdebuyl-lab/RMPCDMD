0b0fe6a8ba78fdac9fe16941806560c3b877cb1e
Pierre de Buyl <pdebuyl@pdebuyl.be>
2018-02-27 14:47:44 +0100

    update rotation_analysis.py

--- experiments/03-single-janus/rotation_analysis.py
+++ experiments/03-single-janus/rotation_analysis.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+"""
+Analyze the rotational motion of a L-shaped colloid.
+"""
+from __future__ import print_function, division
+
 import numpy as np
 import h5py
-
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -14,15 +19,12 @@ parser.add_argument('--n-tiles', type=int, default=2,
                     help='number of tiles for the thickness of the bottom arm')
 args = parser.parse_args()
 
-a = h5py.File(args.file, 'r')
-
-edges = a['particles/janus/box/edges'][:]
-pos = a['particles/janus/position/value'][:]
-im = a['particles/janus/image/value'][:]
-vel = a['particles/janus/velocity/value'][:]
-pos_com = a['observables/janus_pos/value'][:]
-
-a.close()
+with h5py.File(args.file, 'r') as a:
+    edges = a['particles/janus/box/edges'][:]
+    pos = a['particles/janus/position/value'][:]
+    im = a['particles/janus/image/value'][:]
+    vel = a['particles/janus/velocity/value'][:]
+    pos_com = a['observables/janus_pos/value'][:]
 
 vel_com = vel.mean(axis=1)
 
@@ -43,51 +45,45 @@ off_in = np.sum(v12_inplane*one_z, axis=1)
 
 r12 = pos[:,NHALF-2,:] - pos[:,0,:]
 dist12 = np.sqrt(np.sum((pos[0,NHALF-2,:]-pos[0,0,:])**2))
+r12 /= dist12
 
-one_y = np.cross(one_z, r12) / dist12
+one_y = np.cross(one_z, r12)
 
-omega_z = np.sum(v12_inplane*one_y, axis=1) / dist12
+omega_z = np.sum(v12_inplane*one_y, axis=1)
 
 dir_v = np.sum(r12*vel_com, axis=1)
+
 oz_mean = np.mean(omega_z)
+
 print('mean omega_z', oz_mean)
 print('mean directed velocity', dir_v.mean())
 print('rotation radius', dir_v.mean()/oz_mean)
 
 if args.plot:
-    import matplotlib as mpl
-    from mpl_toolkits.mplot3d import Axes3D
     import matplotlib.pyplot as plt
 
     plt.figure()
-    plt.suptitle('directed velocity')
 
-
-    plt.subplot(121)
+    plt.subplot(221)
+    plt.ylabel('directed velocity')
     plt.plot(dir_v)
-    plt.subplot(122)
-    plt.hist(dir_v, bins=32)
+    plt.subplot(222)
+    plt.hist(dir_v, bins=32, normed=True)
     plt.axvline(dir_v.mean(), color='red')
 
+    plt.subplot(223)
+    plt.plot(omega_z)
+    plt.ylabel('rotational velocity')
+    plt.subplot(224)
+    plt.hist(omega_z, bins=32, normed=True)
+    plt.axvline(oz_mean, color='red')
 
     plt.figure()
 
-    plt.suptitle('rotational velocity')
-    plt.subplot(121)
-    plt.plot(omega_z)
-    plt.subplot(122)
-    plt.hist(omega_z, bins=32)
-    plt.axvline(oz_mean, color='red')
-
-    f = plt.figure()
-
     x, y, z = pos_com.T
 
-    ax = f.gca(projection='3d')
-
-    ax.plot(x, y, z)
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
+    plt.plot(x, z)
+    plt.xlabel('x')
+    plt.ylabel('z')
 
     plt.show()
