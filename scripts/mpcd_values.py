1472861b7e3e8af3d38d2807d87cce40d7868193
Pierre de Buyl <pdebuyl@pdebuyl.be>
2018-09-20 09:48:57 +0200

    add transport coefficients for MPC-AT

--- scripts/mpcd_values.py
+++ scripts/mpcd_values.py
@@ -19,6 +19,7 @@ def bulk_command(args):
     parser_bulk.add_argument('--length', type=float, help='Typical length-scale for diffusive time')
     parser_bulk.add_argument('--force', type=float, help='Force for a Poiseuille flow')
     parser_bulk.add_argument('--heigth', type=float, help='Height of the channel for a Poiseuille flow')
+    parser_bulk.add_argument('--AT', help='Enable MPC-AT', action='store_true')
     args = parser_bulk.parse_args(args)
     tau = args.tau
     T = args.T
@@ -27,25 +28,39 @@ def bulk_command(args):
     alpha = args.alpha
     m = 1
     a = 1
-    # Kapral review Eq. 55
-    eta_kin = T * tau * rho / (2*m) * \
-        (5*gamma-(gamma - 1 + np.exp(-gamma))*(2 - np.cos(alpha)-np.cos(2*alpha)))/ \
-        ((gamma - 1 + np.exp(-gamma))*(2 - np.cos(alpha)-np.cos(2*alpha)))
-    # Kapral review Eq. 56
-    eta_coll = m / (18 * a * tau) * (gamma - 1 + np.exp(-gamma))*(1-np.cos(alpha))
+    if args.AT:
+        # Gompper review eq. 57 (times mass density rho)
+        eta_kin = gamma*T*tau/a**3 * (gamma/(gamma-1+np.exp(-gamma)) - 1/2)
+        eta_coll = m*(gamma-1+np.exp(-gamma))/(12*a*tau)
+    else:
+        # Kapral review Eq. 55
+        eta_kin = T * tau * rho / (2*m) * \
+            (5*gamma-(gamma - 1 + np.exp(-gamma))*(2 - np.cos(alpha)-np.cos(2*alpha)))/ \
+            ((gamma - 1 + np.exp(-gamma))*(2 - np.cos(alpha)-np.cos(2*alpha)))
+        # Kapral review Eq. 56
+        eta_coll = m / (18 * a * tau) * (gamma - 1 + np.exp(-gamma))*(1-np.cos(alpha))
     eta = eta_kin + eta_coll
+    print("MPC fluid characterization")
+    print("Dynamics: ", "MPC - AT" if args.AT else "MPCD - SRD")
+    print("--------------------------\n")
+
+    print("Kinetic component: ", eta_kin)
+    print("Collision component: ", eta_coll)
     print("Viscosity", eta)
-    D_fluid = T*tau/(2*m) * (3*gamma/((gamma - 1 + np.exp(-gamma))*(1-np.cos(alpha))) - 1)
-    print("Self-diffusion D", D_fluid)
-    print("Schmidt number Sc", eta/(rho*D_fluid))
-    if args.length:
-        print("Diffusion time", args.length**2/D_fluid)
+    if not args.AT:
+        D_fluid = T*tau/(2*m) * (3*gamma/((gamma - 1 + np.exp(-gamma))*(1-np.cos(alpha))) - 1)
+        print("Self-diffusion D", D_fluid)
+        print("Schmidt number Sc", eta/(rho*D_fluid))
+        if args.length:
+            print("Diffusion time", args.length**2/D_fluid)
     if args.force and args.heigth:
         v_max = v_of_eta(rho, args.force, args.heigth, eta)
         v_av = 2/3*v_max
         print("Flow maximum ", v_max)
         print("Flow average ", v_av)
-        print("Poiseuille flow Peclet number", v_av*args.heigth/D_fluid)
+        if not args.AT:
+            print("Poiseuille flow Peclet number", v_av*args.heigth/D_fluid)
+
 
 if __name__=='__main__':
     bulk_command(sys.argv[1:])
